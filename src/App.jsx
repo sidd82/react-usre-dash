dc0697c4f0b01a50467d9489f7c5c3bca3547b62
sidd82 <psiddhesh14@gmail.com>
2020-04-12 03:10:14 +0530

    Add Functionallity To Click Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const handleSubmit = async () => {
+    try {
+      const response = await Axios.get(`https://api.one-o.in/api/auth/user`, {
+        withCredentials: true,
+        headers: {
+          "x-api-key": process.env.X_API_KEY,
+        },
+      });
+      console.log(response.data);
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  };
   return (
     <div className="App">
-      <button>Click Me</button>
+      <button onClick={handleSubmit} style={{ backgroundColor: "red" }}>
+        Click
+      </button>
     </div>
   );
 }
