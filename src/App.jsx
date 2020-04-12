d2d5d6cb250dd684af22b94528d824f0236d25d5
sidd82 <psiddhesh14@gmail.com>
2020-04-12 06:35:58 +0530

    Added Signout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
 
 function App() {
+  useEffect(() => {
+    window.gapi.load("auth2", function () {
+      window.gapi.auth2.init({
+        client_id: process.env.REACT_APP_AGOOGLE_CLIENT_ID_WEB,
+      });
+    });
+  });
   const handleSubmit = async () => {
     try {
       const response = await Axios.get(`https://api.one-o.in/api/auth/user`, {
@@ -19,8 +26,30 @@ function App() {
   };
   return (
     <div className="App">
-      <button onClick={handleSubmit} style={{ backgroundColor: "red" }}>
+      <button onClick={handleSubmit} style={{ backgroundColor: "yellow" }}>
         Click
+      </button>
+      <button
+        onClick={async () => {
+          const auth2 = window.gapi.auth2.getAuthInstance();
+          if (auth2 != null) {
+            auth2
+              .signOut()
+              .then(auth2.disconnect().then(() => console.log("Success")));
+          }
+          const response = await Axios.get(
+            `https://api.one-o.in/api/auth/user/sign-out`,
+            {
+              withCredentials: true,
+              headers: {
+                "x-api-key": process.env.X_API_KEY,
+              },
+            }
+          );
+          console.log(response);
+        }}
+      >
+        Signout
       </button>
     </div>
   );
